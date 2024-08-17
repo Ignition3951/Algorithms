3332b9298f92b0a4ca7dbc36841bc97880de9d19
Utkarsh <ignition3951@gmail.com>
2024-08-17 12:01:03 +0530

    Implement Insertion Sort

--- Algorithms/src/com/algo/action/Chapter2.java
+++ Algorithms/src/com/algo/action/Chapter2.java
@@ -1,15 +1,18 @@
 package com.algo.action;
 
-import com.algo.service.MergeSortingService;
+import com.algo.service.InsertionSortService;
+import com.algo.service.SelectionSortingService;
 import com.algo.service.SortingService;
 
 public class Chapter2 {
 
 	public static void main(String[] args) {
 		
-		String[] array = SortingService.readFileWithLineSpace("tiny.txt");
-		 MergeSortingService mergeSortingService = new MergeSortingService();
-		 mergeSortingService.sort(array);
+		String[] array = SortingService.readFileWithLineSpace("words3.txt");
+//		 SelectionSortingService selectionSortingService = new SelectionSortingService();
+//		 selectionSortingService.sort(array);
+		InsertionSortService insertionSortService = new InsertionSortService();
+		insertionSortService.sort(array);
 		 SortingService.show(array);
 	}
 }
--- Algorithms/src/com/algo/service/SelectionSortingService.java
+++ Algorithms/src/com/algo/service/SelectionSortingService.java
@@ -1,9 +1,10 @@
 package com.algo.service;
 
-public class MergeSortingService implements SortingService {
+public class SelectionSortingService implements SortingService {
 
 	@Override
 	public void sort(String[] a) {
+		long start=System.currentTimeMillis();
 		int N = a.length;
 		for(int i=0;i<N;i++) {
 			int min=i;
@@ -14,6 +15,8 @@ public class MergeSortingService implements SortingService {
 			}
 			SortingService.exchange(a, i, min);
 		}
+		long end=System.currentTimeMillis();
+		System.out.println("Time elapsed in SelectionSortingService is : "+(end-start));
 	}
 	
 	
