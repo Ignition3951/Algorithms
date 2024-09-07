2c803a3efd17810c77a1e2fb8f239a3280628f6e
Utkarsh <ignition3951@gmail.com>
2024-09-07 12:18:56 +0530

    IMplement shortest path

--- Algorithms/src/com/algo/client/ShortestPathClient.java
+++ Algorithms/src/com/algo/client/ShortestPathClient.java
@@ -2,9 +2,7 @@ package com.algo.client;
 
 import java.util.List;
 
-import com.algo.graph.EdgeWeightedGraph;
 import com.algo.model.DirectedEdge;
-import com.algo.model.Edge;
 import com.algo.model.EdgeWeightedDigraph;
 import com.algo.service.GraphService;
 import com.algo.service.ShortestPath;
@@ -16,6 +14,7 @@ public class ShortestPathClient {
 		System.out.println("Graph has "+array.get(0)+" Vertices and "+array.get(1)+" Edges!!!!");
 		EdgeWeightedDigraph edgeWeightedDigraph = new EdgeWeightedDigraph(Integer.parseInt(array.get(0)));
 		int counter = 2;
+		int source=0;
 		for (int i = 0; i < Integer.parseInt(array.get(1)); i++) {
 			if (counter < array.size()) {
 				int v = Integer.parseInt(array.get(counter++));
@@ -28,15 +27,10 @@ public class ShortestPathClient {
 		for(int i=0;i<Integer.parseInt(array.get(0));i++) {
 			System.out.println("Graph adjacency matrix : "+edgeWeightedDigraph.edges().get(i));
 		}
-		ShortestPath shortestPath = new ShortestPath(edgeWeightedDigraph, 0);
-		for(DirectedEdge edge: shortestPath.edgeTo()) {
-			System.out.println("Shortest Path edge array : "+edge);
-		}
-		for(int i=1;i<edgeWeightedDigraph.vertices();i++) {
+		ShortestPath shortestPath = new ShortestPath(edgeWeightedDigraph, source);
+		for(int i=0;i<edgeWeightedDigraph.vertices();i++) {
 			System.out.print("Edge from 0 to "+i+" is ");
-			for(DirectedEdge directedEdge: shortestPath.pathTo(i)) {
-				System.out.print(directedEdge);
-			}
+			System.out.print(shortestPath.pathTo(i)+" Weight is : "+shortestPath.weight());
 			System.out.println();
 		}
 
