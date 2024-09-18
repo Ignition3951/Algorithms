ddff45164cdac1dd88d607a46b326fa3e1a2510b
Utkarsh <ignition3951@gmail.com>
2024-09-18 13:03:14 +0530

    Code refactor and implement key-indexed-counting

---
+++ Algorithms/src/com/algo/string/service/StringService.java
@@ -0,0 +1,54 @@
+package com.algo.string.service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public interface StringService {
+
+	public static final String NEWLINE = System.getProperty("line.separator");
+
+	static Map<String, Integer> convertFileToStringMap(String inputFilename, String delimiter) {
+		Map<String, Integer> mapOfString = new HashMap<String, Integer>();
+		try (Scanner sc = new Scanner(new File(inputFilename))) {
+			sc.useDelimiter(NEWLINE);
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				String[] nodes = line.split(delimiter);
+				mapOfString.put(nodes[0], Integer.parseInt(nodes[1]));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return mapOfString;
+	}
+
+	static int[] computeFrequencyCounts(Map<String, Integer> mapOfStrings) {
+		int[] count = new int[mapOfStrings.size()];
+		Arrays.fill(count, 0);
+		for (String str : mapOfStrings.keySet()) {
+			count[mapOfStrings.get(str) + 1]++;
+		}
+		return count;
+	}
+
+	static int[] transformCountToIndeces(int[] count, int vertices) {
+		for (int i = 0; i < vertices - 1; i++) {
+			count[i + 1] += count[i];
+		}
+		return count;
+	}
+
+	static String[] distributeTheData(int[] convertedCount, Map<String, Integer> mapOfString) {
+		String[] aux = new String[mapOfString.size()];
+		Arrays.fill(aux, null);
+		for (String key : mapOfString.keySet()) {
+			int value = mapOfString.get(key);
+			aux[convertedCount[value]++] = key;
+		}
+		return aux;
+	}
+}
