c81c2d1f5b6512ca86321fd176e88063d411cf0b
Utkarsh <ignition3951@gmail.com>
2025-06-22 20:54:54 +0530

    Take input with scanner class and print the value

---
+++ Algorithms/src/com/utk/Problem1.java
@@ -0,0 +1,14 @@
+package com.utk;
+
+import java.util.*;
+
+public class Problem1 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the input integer: ");
+
+        Integer number = in.nextInt();
+        System.out.println("The number is: " + number);
+    }
+}
