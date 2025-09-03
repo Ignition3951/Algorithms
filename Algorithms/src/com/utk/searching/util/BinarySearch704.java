3d84b74ffaed4f471184bdf1631de8185623e85e
Utkarsh <ignition3951@gmail.com>
2025-09-03 17:37:16 +0530

    Implement search insert in sorted array

--- Algorithms/src/com/utk/searching/util/BinarySearch704.java
+++ Algorithms/src/com/utk/searching/util/BinarySearch704.java
@@ -13,8 +13,12 @@ public class BinarySearch704 {
         int binarySearchResult = Utility.binarySearch(binarySearchArray, target);
         LOGGER.log(Level.INFO, "The result of the binary search operation is : {0}", binarySearchResult);
 
-        int[] lowerBound = {3,5,8,15,19};
+        int[] lowerBound = {3, 5, 8, 15, 19};
         int lowerBoundTarget = 9;
         LOGGER.log(Level.INFO, "The lower bound for the array is :{0}", Utility.lowerBound(lowerBound, lowerBoundTarget));
+
+        int[] searchInsert = {1, 3};
+        int searchInsertTarget = 2;
+        LOGGER.log(Level.INFO, "The search insert index is : {0}", Utility.searchInsert(searchInsert, searchInsertTarget));
     }
 }
--- Algorithms/src/com/utk/searching/util/Utility.java
+++ Algorithms/src/com/utk/searching/util/Utility.java
@@ -39,4 +39,21 @@ public class Utility {
         }
         return lowerBound;
     }
+
+    public static int searchInsert(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        int mid;
+        int result = arr.length;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (arr[mid] >= target) {
+                result = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return result;
+    }
 }
