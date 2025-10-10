0b3789d061b80a781c2ae98c1b5356bc749403ab
Utkarsh <ignition3951@gmail.com>
2025-10-10 13:24:38 +0530

    Implement detect a loop in linked list

--- Algorithms/src/com/utk/linkedlist/model/Node.java
+++ Algorithms/src/com/utk/linkedlist/model/Node.java
@@ -97,6 +97,27 @@ public class Node {
         return false;
     }
 
+    public Node detectCycle(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+
     @Override
     public String toString() {
         return "Node{" +
