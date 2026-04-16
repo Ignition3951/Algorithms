7776222d98d08a78f2d5a27f568a6c9878a98637
Utkarsh Srivastava <ignition3951@gmail.com>
2026-04-16 22:15:49 +0530

    Stacks and Queues: sumSubArray ranges problem including min and max nge,pge,nse,pse

--- Algorithms/src/com/utk/leetcode/Problem2104.java
+++ Algorithms/src/com/utk/leetcode/Problem2104.java
@@ -3,61 +3,59 @@ package com.utk.leetcode;
 import java.util.Arrays;
 import java.util.Stack;
 
-public class Problem907 {
+public class Problem2104 {
 
     static void main() {
-        int[] arr = new int[]{1,2,3};
-        System.out.println(sumSubarrayMins(arr));
+        int[] arr = {4,-2,-3,4,1};
+        System.out.println(subArrayRanges(arr));
     }
 
-    public static int sumSubarrayMins(int[] arr) {
-        int total = 0;
-        int[] nse = nextSmallerElement(arr);
-        int[] pse = prevSmallerElement(arr);
-        int mod = 1000000007;
+    public static long subArrayRanges(int[] nums) {
+        return sumSubarrayMaxs(nums)-Problem907.sumSubarrayMins(nums);
+    }
+
+    public static long sumSubarrayMaxs(int[] arr) {
+        long total = 0;
+        int[] nge = nextGreaterElement(arr);
+        int[] pge = prevGreaterElement(arr);
         for (int i = 0; i < arr.length; i++) {
             // Count of elements to the left including current
-            int left = i - pse[i];
+            int left = i - pge[i];
 
             // Count of elements to the right including current
-            int right = nse[i] - i;
+            int right = nge[i] - i;
 
             // Total subarrays where arr[i] is the minimum
             long freq = left * right * 1L;
 
             // Contribution = frequency * value
-            int val = (int) ((freq * arr[i]) % mod);
-            total = (total + val) % mod;
+            long val = freq * arr[i];
+            total = (total + val);
         }
         return total;
     }
 
-    public static int[] nextSmallerElement(int[] arr) {
+    public static int[] nextGreaterElement(int[] arr) {
         Stack<Integer> stack = new Stack<>();
         int[] res = new int[arr.length];
-        for (int i = arr.length - 1; i >= 0; i--) {
-
-            while (!stack.isEmpty() && arr[i] <= arr[stack.peek()]) {
+        for (int i = arr.length-1; i >=0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
                 stack.pop();
             }
-
             res[i] = !stack.isEmpty() ? stack.peek() : arr.length;
-
             stack.push(i);
         }
         return res;
     }
 
-    public static int[] prevSmallerElement(int[] arr) {
+    public static int[] prevGreaterElement(int[] arr) {
         Stack<Integer> stack = new Stack<>();
         int[] res = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            while (!stack.isEmpty() && arr[i] < arr[stack.peek()]) {
+        for (int i = 0; i<arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
                 stack.pop();
             }
-
             res[i] = !stack.isEmpty() ? stack.peek() : -1;
-
             stack.push(i);
         }
         return res;
