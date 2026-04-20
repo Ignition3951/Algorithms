0292901ceee8a597579e5e204405b47d60fa5896
Utkarsh Srivastava <ignition3951@gmail.com>
2026-04-20 18:03:41 +0530

    Stacks and Queues: largets rectangle in histogram optimal approach I

--- Algorithms/src/com/utk/leetcode/Problem84.java
+++ Algorithms/src/com/utk/leetcode/Problem84.java
@@ -1,22 +1,50 @@
 package com.utk.leetcode;
 
+import java.util.Stack;
+
 // Largest rectangle in histogram
 public class Problem84 {
 
     static void main() {
-        System.out.println(largestRectangleArea(new int[]{2,1,5,6,2,3}));
+        System.out.println(largestRectangleArea(new int[]{1,1}));
     }
 
     public static int largestRectangleArea(int[] heights) {
-        int maxArea =0;
-        int minValue;
-        for(int i = 0; i<heights.length; i++){
-            minValue = Integer.MAX_VALUE;
-            for(int j=i; j<heights.length; j++){
-                minValue=Math.min(minValue,heights[j]);
-                maxArea=Math.max(maxArea,(j-i+1)*minValue);
-            }
+        int maxArea = 0;
+        int[] pse = preSmallerElement(heights);
+        int[] nse = nextSmallerElement(heights);
+        for (int i = 0; i < heights.length; i++) {
+            int rectangleArea = heights[i] * (nse[i] - (pse[i] + 1));
+            maxArea = Math.max(maxArea, rectangleArea);
         }
         return maxArea;
+    }
+
+    public static int[] preSmallerElement(int[] heights) {
+        int[] pse = new int[heights.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < heights.length; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+
+            pse[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        return pse;
+    }
+
+    public static int[] nextSmallerElement(int[] heights) {
+        int[] nse = new int[heights.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i =heights.length-1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] > heights[i]) {
+                stack.pop();
+            }
+
+            nse[i] = stack.isEmpty() ? heights.length : stack.peek();
+            stack.push(i);
+        }
+        return nse;
     }
 }
