ecbfededacce21cf3da90304cb34b5b2117227be
Utkarsh Srivastava <ignition3951@gmail.com>
2026-04-28 13:16:03 +0530

    Sliding window and two pointer: longest consecutive ones optimal approach

--- Algorithms/src/com/utk/leetcode/Problem1004.java
+++ Algorithms/src/com/utk/leetcode/Problem1004.java
@@ -3,34 +3,29 @@ package com.utk.leetcode;
 public class Problem1004 {
 
     static void main() {
-        int[] arr = {0,0,1,1,1,0,0};
-        int k=0;
+        int[] arr = {0, 0, 1, 1, 1, 0, 0};
+        int k = 0;
         System.out.println(longestOnes(arr, k));
     }
 
     public static int longestOnes(int[] nums, int k) {
-        int counter=0;
-        int  left=0;
-        int right=0;
         int result = 0;
-        while(left<=right && right<nums.length){
-            if(nums[right]==1){
-                right++;
-            }else if(nums[right]==0 && counter<k){
-                right++;
-                counter++;
-            }else if(left!=right){
-                if(nums[left]==1){
-                    left++;
-                }else{
-                    left++;
-                    counter--;
-                }
-            }else{
-                left++;
-                right++;
+        int left = 0;
+        int right = 0;
+        int zeroes = 0;
+        while (right < nums.length) {
+            if (nums[right] == 0) {
+                zeroes++;
             }
-            result = Math.max(result, right-left);
+            if (zeroes > k) {
+                if (nums[left] == 0) zeroes--;
+                left++;
+            }
+            if (zeroes <= k) {
+                int length = right - left + 1;
+                result = Math.max(length, result);
+            }
+            right++;
         }
         return result;
     }
