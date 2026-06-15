54beb3ba22133171cc3cdab592d2c5d0d7bf1b6b
Utkarsh Srivastava <ignition3951@gmail.com>
2026-06-15 23:16:27 +0530

    Heap: Prblem 276 tuf minCost to connect sticks use minHeap

--- Algorithms/src/com/utk/tuf/Problem276.java
+++ Algorithms/src/com/utk/tuf/Problem276.java
@@ -1,31 +1,33 @@
 package com.utk.tuf;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Problem276 {
 
     static void main() {
         List<Integer> list = Arrays.asList(1, 8, 3, 5);
-        Collections.sort(list);
         System.out.println(connectSticks(list));
     }
 
     public static int connectSticks(List<Integer> sticks) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int stick : sticks) {
+            pq.offer(stick);
+        }
         int minCost = 0;
-        int pointer = sticks.size() - 1;
-        while (pointer > 0) {
-            minCost += sum(sticks, pointer--);
+        while (pq.size() > 1) {
+            int firstSmallest = pq.poll();
+            int secondSmallest = pq.poll();
+
+            int count = firstSmallest + secondSmallest;
+
+            minCost += count;
+            pq.offer(count);
+
         }
         return minCost;
     }
 
-    public static int sum(List<Integer> sticks, int pointer) {
-        int sum = 0;
-        for (int i = pointer; i >= 0; i--) {
-            sum += sticks.get(i);
-        }
-        return sum;
-    }
 }
